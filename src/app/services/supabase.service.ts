a23a9dcfbc21a2a15977aed3e658d432d0f9e281
Juan Barbosa <106997745+JuanBM0308@users.noreply.github.com>
2026-05-10 12:04:58 -0500

    Delete src/app/services/supabase.service.ts

--- src/app/services/supabase.service.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { environment } from '@env/environment.prod';
-
-export const supabase = createClient(environment.supabaseUrl, environment.supabaseKey, {
-    auth: { persistSession: false },
-    global: {
-        headers: { 'Accept': 'application/json' },
-    },
-});
